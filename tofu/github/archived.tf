a5ed0a52b1ae5c1f0a16e2cad05c4e6f5eb6e016
yutakobayashidev <hi@yutakobayashi.com>
2026-04-08 05:34:06 +0900

    feat: manage archived daily-report repository

--- tofu/github/archived.tf
+++ tofu/github/archived.tf
@@ -15,3 +15,21 @@ resource "github_repository" "grafanaconf" {
 
   delete_branch_on_merge = true
 }
+
+resource "github_repository" "daily_report" {
+  name        = "daily-report"
+  visibility  = "public"
+  archived    = true
+  description = "Archived: use ryoppippi/gh-nippou instead"
+
+  has_issues    = true
+  has_projects  = true
+  has_wiki      = true
+  has_downloads = true
+
+  allow_merge_commit = true
+  allow_squash_merge = true
+  allow_rebase_merge = true
+
+  delete_branch_on_merge = false
+}
